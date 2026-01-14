ec8daab4f2c89a6285616890af3890dcc826afee
chougaleaishwarya453-cloud <chougaleaishwarya453@gmail.com>
2026-01-14 16:22:07 +0530

    Add files via upload

---
+++ assignment5.py
@@ -0,0 +1,16 @@
+name = input("Enter your name: ")
+age = int(input("Enter your age: "))
+
+mark1 = int(input("Enter marks for subject 1: "))
+mark2 = int(input("Enter marks for subject 2: "))
+mark3 = int(input("Enter marks for subject 3: "))
+
+
+average_marks = (mark1 + mark2 + mark3) / 3
+
+
+print("\nName:", name)
+print("Age:", age)
+print("Average Marks:", average_marks)
+
+
