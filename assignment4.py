4ad28afe8750753db9f887d661563e25ae5cf7cc
chougaleaishwarya453-cloud <chougaleaishwarya453@gmail.com>
2026-01-14 16:43:02 +0530

    Add files via upload

---
+++ assignment4.py
@@ -0,0 +1,14 @@
+price = input("Enter the price of the item: ")
+
+
+quantity = input("Enter the quantity: ")
+
+
+price = float(price)
+quantity = int(quantity)
+
+
+total_cost = price * quantity
+
+print("Total cost:", total_cost)
+
