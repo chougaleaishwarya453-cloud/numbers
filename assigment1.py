b73858747435d7e588869ec5a33c35b86f7566ad
chougaleaishwarya453-cloud <chougaleaishwarya453@gmail.com>
2026-01-14 16:22:40 +0530

    Add files via upload

---
+++ assigment1.py
@@ -0,0 +1,10 @@
+
+name = input("Enter your name: ")
+favorite_number = input("Enter your favorite number: ")
+
+
+print(f"Hello {name}, your favorite number is {favorite_number}.")
+
+
+print("Data type of name:", type(name))
+print("Data type of favorite_number:", type(favorite_number))
